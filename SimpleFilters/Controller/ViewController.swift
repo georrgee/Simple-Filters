525bfc4658f405c7e80706511c05f4329c67b62f
George Garcia <garcia.george.b@gmail.com>
2019-04-10 14:55:09 -0700

    - added rounded corners for the buttons - fixed up the disappearance image issue

--- SimpleFilters/Controller/ViewController.swift
+++ SimpleFilters/Controller/ViewController.swift
@@ -9,16 +9,35 @@ import CoreImage
 
 class ViewController: UIViewController {
     
+    
+    // MARK: IBOutlets
     @IBOutlet weak var imageView: UIImageView?
+    @IBOutlet weak var sepiaButtonView: UIView!
+    @IBOutlet weak var blurButtonView: UIView!
+    @IBOutlet weak var noirButtonView: UIView!
+    @IBOutlet weak var processEffectView: UIView!
+    @IBOutlet weak var resetButtonView: UIView!
+    
+    
+    // MARK: Properties
     private var originalImage: UIImage?
     
+    
+    // MARK: IBActions
     @IBAction func applySepiaFilter(_ sender: Any) {
         
         guard let image = imageView?.image else {
             return
         }
         
-        imageView?.image = applyFilterTo(image: image, filterEffect: Filter(filterName: "CISepiaTone", filterEffectValue: 0.85, filterEffectValueName: kCIInputIntensityKey))
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let self = self else {
+                return
+            }
+            DispatchQueue.main.async { [weak self] in
+                self?.imageView?.image = self?.applyFilterTo(image: image, filterEffect: Filter(filterName: "CISepiaTone", filterEffectValue: 0.85, filterEffectValueName: kCIInputIntensityKey))
+            }
+       }
     }
     
     @IBAction func applyPhotoTransferEffect(_ sender: Any) {
@@ -42,7 +61,17 @@ class ViewController: UIViewController {
         guard let image = imageView?.image else {
             return
         }
-        imageView?.image = applyFilterTo(image: image, filterEffect: Filter(filterName: "CIPhotoEffectNoir", filterEffectValue: nil, filterEffectValueName: nil))
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            
+            guard let self = self else {
+                return
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.imageView?.image = self?.applyFilterTo(image: image, filterEffect: Filter(filterName: "CIPhotoEffectNoir", filterEffectValue: nil, filterEffectValueName: nil))
+            }
+        }
     }
     
     @IBAction func applyBlurEffect(_ sender: Any) {
@@ -51,53 +80,117 @@ class ViewController: UIViewController {
             return
         }
         
-        imageView?.image = applyFilterTo(image: image, filterEffect: Filter(filterName: "CIGaussianBlur", filterEffectValue: 8.0, filterEffectValueName: kCIInputRadiusKey))
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            
+            guard let self = self else {
+                return
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                   self?.imageView?.image = self?.applyFilterTo(image: image, filterEffect: Filter(filterName: "CIGaussianBlur", filterEffectValue: 8.0, filterEffectValueName: kCIInputRadiusKey))
+            }
+        }
     }
     
     @IBAction func clearFilters(_ sender: Any) {
-        imageView?.image = originalImage
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let self = self else {
+                return
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.imageView?.image = self?.originalImage
+            }
+        }
     }
     
+    // MARK: View Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        originalImage = imageView?.image
+        setupButtonUI()
+        
+    }
+    
+    // MARK: Private Methods
     private func applyFilterTo(image: UIImage, filterEffect: Filter) -> UIImage? {
         
-        // Got a CGImage from our UIImage, contains an optional CGImage
-        // We try to get the graphics context, an openGLContext, by creating one and making sure we can create one
+        // 1
         guard let cgImage = image.cgImage, let openGLContext = EAGLContext(api: .openGLES3) else { return nil }
-        
-        // creating a Core Image Context, feeding the graphics context here
+        // 2
         let context = CIContext(eaglContext: openGLContext)
         
         let ciImage = CIImage(cgImage: cgImage)
         
-        // Created a filter var for the given filter name
+        // 3
         let filter = CIFilter(name: filterEffect.filterName)
         
-        // "hey filter, this the image you are going to use to apply filters on
+        // 4
         filter?.setValue(ciImage, forKey: kCIInputImageKey)
         
-        // did a check to see if we have an values i.e.  input intensity, Gaussian Blur(radius effect)
+       // 5
         if let filterEffectValue = filterEffect.filterEffectValue,
             let filterEffectValueName = filterEffect.filterEffectValueName {
             
             filter?.setValue(filterEffectValue, forKey: filterEffectValueName)
-            
         }
         
-        // finally we render the output image
+        // 6
         var filteredImage: UIImage?
         
-        // hey filter give me the value for the output image key as a CIImage, then create a CGImage from the output and then finally render it into a UIImage
+        // 7
         if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage,
             let cgiImageResult = context.createCGImage(output, from: output.extent) {
             filteredImage = UIImage(cgImage: cgiImageResult)
         }
-        
         return filteredImage
+    }
+    
+    private func setupButtonUI() {
+        
+        sepiaButtonView.layer.cornerRadius = 10
+        sepiaButtonView.clipsToBounds = true
+        
+        noirButtonView.layer.cornerRadius = 10
+        noirButtonView.clipsToBounds = true
+        
+        blurButtonView.layer.cornerRadius = 10
+        blurButtonView.clipsToBounds = true
+        
+        processEffectView.layer.cornerRadius = 10
+        processEffectView.clipsToBounds = true
+        
+        resetButtonView.layer.cornerRadius = 10
+        resetButtonView.clipsToBounds = true
     }
     
 }
 
+
+// MARK: NOTES
+
+
+/* | applyFilter Method | (Lines: 114-146)
+ 
+ 1) Got a CGImage from our UIImage, contains an optional CGImage
+    We try to get the graphics context, an openGLContext,
+    by creating one and making sure we can create one
+ 
+ 2) Creating a Core Image Context, feeding the graphics context here
+ 
+ 3) Created a filter variable for the given filter name
+
+ 4) Pretty much saying: "hey filter, this the image
+    you are going to use to apply filters on"
+ 
+ 5) Check to see if we have an values
+     example:  input intensity, Gaussian Blur(radius effect)
+ 
+ 6) Finally we render the output image
+
+ 7) Saying: "Hey filter! Give me the value for the output image key
+    as a CIImage, then create a CGImage from the output and
+    then finally render it into a UIImage
+*/
